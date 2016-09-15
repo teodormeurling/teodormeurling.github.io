f51f084d7006abd99020d4debc47fdcfc6a777ec
Teodor Meurling <teodor.meurling@gmail.com>
2016-09-15 17:15:53 +0200

    aa
    aaa

--- js/datalandscape.js
+++ js/datalandscape.js
@@ -11,12 +11,3 @@ new Chartist.Line('.ct-chart', {
     right: 40
   }
 });
-
-new Chartist.Line('#chart1', {
-  labels: ['monday', 'tuesday', 'wednsday', 'thursday', 'friday'],
-  series: [[100, 86, 40, 20, 25, 30]
-  ]
-  }, {
-low: 0,
-showArea: true
-});
