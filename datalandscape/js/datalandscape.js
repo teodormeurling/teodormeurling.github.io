f2e19a6b7f105d2e53fb30eda5fcf905144e420f
Teodor Meurling <teodor.meurling@gmail.com>
2016-09-15 17:26:48 +0200

    more
    aaa

--- datalandscape/js/datalandscape.js
+++ datalandscape/js/datalandscape.js
@@ -1,13 +1,9 @@
-new Chartist.Line('.ct-chart', {
-  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-  series: [
-    [12, 9, 7, 8, 5],
-    [2, 1, 3.5, 7, 3],
-    [1, 3, 4, 5, 6]
+// Initialize a Line chart in the container with the ID chart1
+new Chartist.Line('#chart1', {
+  labels: ['monday', 'tuesday', 'wednsday', 'thursday', 'friday'],
+  series: [[100, 86, 40, 20, 25, 30]
   ]
-}, {
-  fullWidth: true,
-  chartPadding: {
-    right: 40
-  }
+  }, {
+low: 0,
+showArea: true
 });
